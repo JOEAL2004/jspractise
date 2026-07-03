3ae2b400ed43cfdb096046bafc9ae64a07a231b9
JOEAL2004 <joealjames610@gmail.com>
2026-07-03 11:52:22 +0530

    Practise arthimatic operators

---
+++ operator.js
@@ -0,0 +1,9 @@
+const a=20;
+const b=6;
+
+console.log("Addeition",a+b);
+console.log('Subtraction',a-b);
+console.log('Multiplication',a*b);
+console.log("Diveision",a/b);
+console.log("Modulus",a%b);
+console.log("Exponentiation",a**b);
