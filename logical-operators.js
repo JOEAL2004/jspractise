d5a246df4c7f1b4ff74478354cd1c699ea8d9a29
JOEAL2004 <joealjames610@gmail.com>
2026-07-03 20:59:21 +0530

    Practised logical operators

---
+++ logical-operators.js
@@ -0,0 +1,9 @@
+const age=21;
+const hasLicense=true;
+const isStudent=false;
+
+
+console.log(age>=18 && hasLicense);
+console.log(age>=18 || isStudent);
+console.log(!isStudent);
+console.log(hasLicense && !isStudent);
