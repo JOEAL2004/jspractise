a11be14cde01c9b2802a59e87b8cf5db4a7a245c
JOEAL2004 <joealjames610@gmail.com>
2026-07-03 11:35:07 +0530

    Practise javascript variables

---
+++ practise.js
@@ -0,0 +1,17 @@
+const name="joeal";
+const age=21;
+const isstudent=true;
+let score;
+const country=null;
+
+console.log(name);
+console.log(age);
+console.log(isstudent);
+console.log(score);
+console.log(country);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isstudent);
+console.log(typeof score);
+console.log(typeof country);
