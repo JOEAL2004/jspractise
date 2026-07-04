a1737eefb7ddfc6922961dca9fbcdf1a5c14dc9c
JOEAL2004 <joealjames610@gmail.com>
2026-07-04 10:17:25 +0530

    Learn function parameters and arguments

--- function.js
+++ function.js
@@ -2,3 +2,18 @@ function greet(){
   console.log("Hello, Joeal");
 }
 greet();
+
+function showName(){
+  console.log("Joeal Rapheal James");
+}
+function showAge(){
+  console.log("21");
+}
+
+function showCollege(){
+  console.log("Government College of Engineering,Kannur");
+}
+
+showName();
+showAge();
+showCollege();
---
+++ parameters.js
@@ -0,0 +1,14 @@
+function greet(name){
+  console.log("Hello,"+name);
+}
+greet("Joeal");
+greet("Meenu");
+greet("Jessy");
+
+
+function add(a,b){
+  console.log(a+b);
+}
+
+add(10,20);
+add(30,-9);
