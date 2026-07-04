6925fda536fd07716a064338ff59be6c7b49327a
JOEAL2004 <joealjames610@gmail.com>
2026-07-04 10:30:00 +0530

    Practised return values

--- parameters.js
+++ parameters.js
@@ -12,3 +12,12 @@ function add(a,b){
 
 add(10,20);
 add(30,-9);
+
+function introduce(name,age,college){
+  console.log("My name is "+name);
+  console.log("I am "+age+" years old");
+  console.log("I study in "+college);
+}
+
+introduce("Joeal",21,"Government College of Engineering,Kannur");
+introduce("Meenu",20,"Government College of Engineering,Kannur");
---
+++ return-values.js
@@ -0,0 +1,11 @@
+function add(a,b){
+  return a+b;
+}
+const sum=add(10,20);
+console.log(sum);
+
+function square(n)
+{
+  return n*n;
+}
+console.log(square(2));
