22da8d300fc65f8dd281ed01d76db3c4e69925da
JOEAL2004 <joealjames610@gmail.com>
2026-07-05 12:32:13 +0530

    Learn javascript DOM Basics

---
+++ dom-practise/script.js
@@ -0,0 +1,9 @@
+const title=document.querySelector('#title');
+const heading=document.querySelector('#heading');
+title.textContent="Learning DOM";
+title.style.color="purple";
+title.style.fontSize="50px";
+heading.textContent="Welcome Meenu";
+heading.style.color="blue";
+heading.style.fontSize="48px";
+console.log(title);
